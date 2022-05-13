d2acab323e8899a56fe91b654f774d3060ab8b55
ChVictoria <98973137+ChVictoria@users.noreply.github.com>
2022-05-13 09:10:08 +0200

    Update Lab3_new.java

--- Lab3_new.java
+++ Lab3_new.java
@@ -16,18 +16,18 @@ public class Lab3_new {
             StringBuffer a = new StringBuffer(scan.nextLine());
             text.append(a);
         }
-        String[] sentences = text.toString().split("(?<=[.?!]{1,3}) "); //"(?<=([.?!]+)) " ?? подумай
-        String[][] splitted_sentences = new String[sentences.length][];
+        String[] sentences = text.toString().split("(?<=[.?!]{1,3}) +");
+        String[][] splittedSentences = new String[sentences.length][];
         for (int i = 0; i < sentences.length; i++) {
-            splitted_sentences[i] = sentences[i].split("([ -]+)");
+            splittedSentences[i] = sentences[i].split("(( - )| +)");
         }
-        Arrays.sort(splitted_sentences, Comparator.comparingInt(o -> o.length));
-        StringBuffer final_text = new StringBuffer();
-        for (String[] splitted_sentence : splitted_sentences) {
-            for (String s : splitted_sentence) {
-                final_text.append(s).append(" ");
+        Arrays.sort(splittedSentences, Comparator.comparingInt(o -> o.length));
+        StringBuffer finalText = new StringBuffer();
+        for (String[] splittedSentence : splittedSentences) {
+            for (String s : splittedSentence) {
+                finalText.append(s).append(" ");
             }
         }
-        System.out.println(final_text);
+        System.out.println(finalText);
     }
 }
